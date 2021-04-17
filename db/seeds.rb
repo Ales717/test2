e9522d4c21e4b4ab00171eddabcc4e9ee4fbfcdd
Ales717 <prosenjak.ales77@gmail.com>
2021-04-17 09:58:24 +0200

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 movie_seed = JSON.parse(RestClient.get("https://api.themoviedb.org/3/movie/popular?api_key=#{ENV["MOVIE_API_KEY"]}&page=1"))
 movie_seed['results'].each do |movie|
-  Movie.create(
+  Movie.where(
     movie_id: movie['id'],
     title: movie['title'],
     release_date: movie['release_date'],
@@ -16,12 +16,12 @@ movie_seed['results'].each do |movie|
     backdrop_path: movie['backdrop_path'],
     overview: movie['overview'],
     average_vote: movie['vote_average']
-  )
+  ).first_or_create
 end
 
 trailer_seed = JSON.parse(RestClient.get("https://youtube.googleapis.com/youtube/v3/search?channelId=UCT0hbLDa-unWsnZ6Rjzkfug&key=#{ENV["YOUTUBE_API_KEY"]}"))
 trailer_seed['items'].each do |trailer|
-  Trailer.create(
+  Trailer.where(
     videoId: trailer['id']['videoId']
-  )
+  ).first_or_create
 end
